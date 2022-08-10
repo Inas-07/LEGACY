345f5090a723b733a4065127b0445f4999ef301b
Inas-07 <42310741+Inas-07@users.noreply.github.com>
2022-08-10 18:38:47 +0800

    Added close door event

--- LEGACY.Utilities.cs
+++ LEGACY.Utilities.cs
@@ -179,6 +179,30 @@ namespace LEGACY.Utilities
             if (zone.m_sourceGate == null) return false;
             return zone.m_sourceGate.SpawnedDoor.LastStatus == eDoorStatus.Open;
         }
+
+        public static bool TryGetZoneEntranceSecDoor(LG_Zone zone, out LG_SecurityDoor door)
+        {
+            if (zone == null)
+            {
+                Logger.Error("Zone was Null!");
+                door = null;
+                return false;
+            }
+            if (zone.m_sourceGate == null)
+            {
+                Logger.Error("Entrace Gate is Null!");
+                door = null;
+                return false;
+            }
+            if (zone.m_sourceGate.SpawnedDoor == null)
+            {
+                Logger.Error("SpawnedDoor is Null!");
+                door = null;
+                return false;
+            }
+            door = zone.m_sourceGate.SpawnedDoor.TryCast<LG_SecurityDoor>();
+            return door != null;
+        }
     }
 
     public enum MainLayerID 
--- Patch_ExtraEventsConfig.cs
+++ Patch_ExtraEventsConfig.cs
@@ -9,6 +9,11 @@ using LEGACY.Utilities;
 
 namespace LEGACY.Patch
 {
+    enum EventType
+    {
+        CloseSecurityDoor_Custom = 100,
+    }
+
     [HarmonyPatch]
     class Patch_ExtraEventsConfig
     {
@@ -58,42 +63,43 @@ namespace LEGACY.Patch
             return false;
         }
 
-        //private static bool LockSecurityDoor_Custom(WardenObjectiveEventData eventToTrigger, eWardenObjectiveEventTrigger trigger)
-        //{
-        //    // -===============================
-        //    //      some research
-        //    // -===============================
-        //    //LG_LayerType layer = eventToTrigger.Layer;
-        //    //eLocalZoneIndex localIndex = eventToTrigger.LocalIndex;
-        //    //eDimensionIndex dimensionIndex = eventToTrigger.DimensionIndex;
-        //    //LG_Zone zoneToBeLocked = null;
-        //    //Builder.CurrentFloor.TryGetZoneByLocalIndex(dimensionIndex, layer, localIndex, out zoneToBeLocked);
-        //    //if (zoneToBeLocked == null)
-        //    //{
-        //    //    Logger.Error("Failed to get target zone {0}, layer {1}, dimension {2}.", localIndex, layer, dimensionIndex);
-        //    //    return true;
-        //    //}
+        private static void CloseSecurityDoor_Custom(WardenObjectiveEventData eventToTrigger, eWardenObjectiveEventTrigger trigger)
+        {
+            LG_Zone zone = null;
+            if(Builder.CurrentFloor.TryGetZoneByLocalIndex(eventToTrigger.DimensionIndex, eventToTrigger.Layer, eventToTrigger.LocalIndex, out zone) == false || zone == null)
+            {
+                Logger.Error("CloseSecurityDoor_Custom: Failed to get zone {0}, layer {1}, dimensionIndex {2}", eventToTrigger.LocalIndex, eventToTrigger.Layer, eventToTrigger.DimensionIndex);
+                return ;
+            }
+
+            LG_SecurityDoor door = null;
+            if(Utilities.Utils.TryGetZoneEntranceSecDoor(zone, out door) == false || door == null)
+            {
+                Logger.Error("CloseSecurityDoor_Custom: failed to get LG_SecurityDoor!");
+                return;
+            }
+
+            pDoorState currentSyncState1 = door.m_sync.GetCurrentSyncState();
+            if (currentSyncState1.status != eDoorStatus.Open && currentSyncState1.status != eDoorStatus.Opening)
+                return;
+            Logger.Debug("Door Closed!");
+            LG_Door_Sync lgDoorSync = door.m_sync.TryCast<LG_Door_Sync>();
             
-        //    //zoneToBeLocked.m_sourceGate.SpawnedDoor.
+            if (lgDoorSync == null) return;
+            
+            pDoorState currentSyncState2 = lgDoorSync.GetCurrentSyncState() with
+            {
+                status = eDoorStatus.Closed,
+                hasBeenOpenedDuringGame = false
+            };
 
-        //    // -===============================
-        //    // possible implementation.
-        //    // -===============================
-        //    //LG_LayerType layer = eventToTrigger.Layer;
-        //    //eLocalZoneIndex localIndex = eventToTrigger.LocalIndex;
-        //    //eDimensionIndex dimensionIndex = eventToTrigger.DimensionIndex;
-        //    //LG_Zone zoneToBeLocked = null;
-        //    //Builder.CurrentFloor.TryGetZoneByLocalIndex(dimensionIndex, layer, localIndex, out zoneToBeLocked);
-        //    //if (zoneToBeLocked == null)
-        //    //{
-        //    //    Logger.Error("Failed to get target zone {0}, layer {1}, dimension {2}.", localIndex, layer, dimensionIndex);
-        //    //    return true;
-        //    //}
+            lgDoorSync.m_stateReplicator.State = currentSyncState2;
+            LG_Gate gate = door.Gate;
+            gate.HasBeenOpenedDuringPlay = false;
+            gate.IsTraversable = false;
 
-        //    //Builder.CurrentFloor
-
-        //    return true;
-        //}
+            WardenObjectiveManager.DisplayWardenIntel(eventToTrigger.Layer, eventToTrigger.WardenIntel);
+        }
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(WardenObjectiveManager), nameof(WardenObjectiveManager.CheckAndExecuteEventsOnTrigger), new System.Type[] {
@@ -108,13 +114,17 @@ namespace LEGACY.Patch
             bool ignoreTrigger = false,
             float currentDuration = 0.0f)
         {
-            switch(eventToTrigger.Type)
+            switch((int)eventToTrigger.Type)
             {
-                case eWardenObjectiveEventType.SetTerminalCommand:
+                case (int)eWardenObjectiveEventType.SetTerminalCommand:
                     return SetTerminalCommand_Custom(eventToTrigger, trigger);
 
-                //case eWardenObjectiveEventType.LockSecurityDoor:
-                //    return LockSecurityDoor_Custom(eventToTrigger, trigger);
+                case (int)EventType.CloseSecurityDoor_Custom:
+                    if (SNetwork.SNet.IsMaster)
+                    {
+                        CloseSecurityDoor_Custom(eventToTrigger, trigger);
+                    }
+                    return false;
 
                 default: return true;
             }
